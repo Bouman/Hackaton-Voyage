afe79500f0bb24349c8f66498d0bfda12f8c7a5a
STEPHANIE MALBERT <[STEPHANIE.MALBERT@live.fr](mailto:STEPHANIE.MALBERT@live.fr)>
2022-11-25 13:48:11 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -37,4 +37,43 @@ const [openMouseOver, setOpenMouseOver] = useState(null);
   );
 }
 
+export default App;import React, { useState } from "react";
+import Carte from "./pages/Carte";
+import './assets/css/App.css';
+import Context from './contexts/Context';
+import monumentList from "./Data/MonumentList";
+import ModaleMonuments from "./components/ModaleMonuments";
+import "./assets/css/App.css";
+import title from "./assets/img/title_reverse.png";
+
+//rotation du titre
+function handleMouseOver(e) {
+  e.target.style.transform = "rotate(180deg)";
+}
+
+function handleMouseLeave(e) {
+  e.target.style.transform = "translate(0,0)";
+}
+
+function App() {
+
+const [invert, setInvert] = useState(true);
+const [moveMap, setmoveMap] = useState(true);
+const [openMouseOver, setOpenMouseOver] = useState(null);
+
+  return (
+    <Context.Provider value={{ moveMap, setmoveMap, openMouseOver, setOpenMouseOver }}>
+      <div className="App">
+        <div className="container">
+          <header className={invert ? "header" : "footer"}>
+            <img src={title} alt="Inversion du titre" className="img"/>
+          </header>
+            <Carte />
+          <footer className={invert? "footer" : "header"}>Hackathon - 2022</footer>
+        </div>
+      </div>
+    </Context.Provider>
+  );
+}
+
 export default App;
